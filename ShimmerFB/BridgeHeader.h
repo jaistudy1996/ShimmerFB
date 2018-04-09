b4b98c61e3f3b2584c92961b51fcefac48eeaa77
Jayant Arora <jaistudy1996@gmail.com>
2018-04-09 13:06:22 -0400

    Sample test with FB shimmer

---
+++ ShimmerFB/BridgeHeader.h
@@ -0,0 +1,15 @@
+//
+//  BridgeHeader.h
+//  ShimmerFB
+//
+//  Created by Jayant Arora on 4/9/18.
+//  Copyright © 2018 Jayant Arora. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+
+#endif /* BridgeHeader_h */
+
+#import <Shimmer/FBShimmering.h>
--- ShimmerFB/ViewController.swift
+++ ShimmerFB/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Shimmer
 
 class ViewController: UIViewController {
 
+    @IBOutlet var shimmerView: FBShimmeringView!
+    @IBOutlet var timeLabel: UILabel!
+    var updateTimer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Initial update
+        initialSetup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func initialSetup(){
+        updateTimer = Timer.scheduledTimer(timeInterval: 1.0,
+                                           target: self,
+                                           selector: #selector(updateTime),
+                                           userInfo: nil,
+                                           repeats: true)
+
+        shimmerView?.contentView = timeLabel
+        shimmerView.shimmeringPauseDuration = 0.2
+        shimmerView?.isShimmering = true
+
+    }
+
+    @objc func updateTime() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "en_US")
+        timeLabel.text = dateFormatter.string(from: Date())
+    }
 
 }
 
